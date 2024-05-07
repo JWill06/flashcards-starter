a0ea7b9743475111b777c2d041eaee938619b264
jordan williamson <williamsonjordan05@gmail.com>
2024-05-06 19:20:15 -0600

    started test files and created new files for iteration 2

---
+++ src/card.js
@@ -0,0 +1,24 @@
+function createCard(id, question, answers, correctAnswer ){
+    let cards = {
+        id: id,
+        question: question,
+        answers: answers,
+        correctAnswer: correctAnswer
+    }
+    return cards
+}
+
+function evaluateGuess(guess, correctAnswer){
+    if(guess === correctAnswer){
+        console.log('Correct!')
+        return `Correct!`
+    } else {
+        console.log('Incorrect!')
+        return `Incorrect!`
+    }
+}
+
+module.exports = {
+    createCard,
+    evaluateGuess,
+}
